7219a92608351977315dbc55259451be78c16fa1
ivz-dev <zakutynsky@yahoo.com>
2017-05-29 10:46:28 +0300

    fixed bug

--- src/routes/synthesizesTextToAudio.php
+++ src/routes/synthesizesTextToAudio.php
@@ -33,78 +33,53 @@ $app->post('/api/IBMWatsonTTS/synthesizesTextToAudio', function ($request, $resp
 
     $client = $this->httpClient;
 
-    $result = [];
-
-    $client->postAsync($query_str,
-        [
-            'auth' => $auth,
-            'query' => $query,
+    try {
+        $resp = $client->post($query_str, [
             'headers' => $headers,
-            'json' => $body,
-            'stream' => true
-        ]
-    )
-        ->then(
-            function (\Psr\Http\Message\ResponseInterface $response) use ($client, $post_data, $settings, &$result) {
-                $responseApi = $response->getBody()->getContents();
+            "auth" => $auth,
+            "json" => $body,
+            "stream" => 'true'
+        ]);
+        $responseBody = $resp->getBody()->getContents();
 
-                $size = strlen($responseApi);
-                var_dump($responseApi);
-                die();
-
-                if (in_array($response->getStatusCode(), ['200', '201', '202', '203', '204'])) {
-                    try {
-                        $fileUrl = $client->post($settings['uploadServiceUrl'], [
-                            'multipart' => [
-                                [
-                                    'name' => 'length',
-                                    'contents' => $size
-                                ],
-                                [
-                                    'name' => 'file',
-                                    'filename' => bin2hex(random_bytes(5)) . $settings['fileExtensions']['wav'],
-                                    'contents' => $responseApi
-                                ],
-                            ]
-                        ]);
-
-                        $gcloud = $fileUrl->getBody()->getContents();
-
-                        $resultDecoded = json_decode($gcloud, true);
-
-                        $result['callback'] = 'success';
-                        $result['contextWrites']['to'] = ($resultDecoded != NULL) ? $resultDecoded : $gcloud;
-                    } catch (GuzzleHttp\Exception\BadResponseException $exception) {
-                        $result['callback'] = 'error';
-                        $result['contextWrites']['to']['status_code'] = 'INTERNAL_PACKAGE_ERROR';
-                        $result['contextWrites']['to']['status_msg'] = 'Something went wrong during file link receiving.';
-                    }
-
-                } else {
-                    $resultDecoded = json_decode($responseApi, true);
-
-                    $result['callback'] = 'error';
-                    $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-                    $result['contextWrites']['to']['status_msg'] = ($resultDecoded != NULL) ? $resultDecoded : $responseApi;
-
-                }
-            },
-            function (GuzzleHttp\Exception\BadResponseException $exception) use (&$result) {
-
+        if ($resp->getStatusCode() == 200) {
+            $size = $resp->getHeader('Content-Length')[0];
+            $contentDisposition = $resp->getHeader('Content-Disposition')[0];
+            $contentDisposition = str_replace("attachment", "", $contentDisposition);
+            $contentDisposition = str_replace('filename=', "", $contentDisposition);
+            $contentDisposition = str_replace(';', "", $contentDisposition);
+            $uploadServiceResponse = $client->post($settings['uploadServiceUrl'], [
+                'multipart' => [
+                    [
+                        "name" => "file",
+                        "filename" => trim($contentDisposition),
+                        "contents" => $responseBody
+                    ],
+                    [
+                        'name' => 'length',
+                        'contents' => $size
+                    ]
+                ]
+            ]);
+            $uploadServiceResponseBody = $uploadServiceResponse->getBody()->getContents();
+            if ($uploadServiceResponse->getStatusCode() == 200) {
+                $result['callback'] = 'success';
+                $result['contextWrites']['to'] = json_decode($uploadServiceResponse->getBody());
+            }
+            else {
                 $result['callback'] = 'error';
                 $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-                $result['contextWrites']['to']['status_msg'] = $exception->getMessage();
-
-            },
-            function (GuzzleHttp\Exception\ConnectException $exception) use (&$result) {
-                $result['callback'] = 'error';
-                $result['contextWrites']['to']['status_code'] = 'INTERNAL_PACKAGE_ERROR';
-                $result['contextWrites']['to']['status_msg'] = 'Something went wrong inside the package.';
-
+                $result['contextWrites']['to']['status_msg'] = is_array($uploadServiceResponseBody) ? $uploadServiceResponseBody : json_decode($uploadServiceResponseBody);
             }
-        )
-        ->wait();
-
-    return $response->withHeader('Content-type', 'application/json')->withJson($result, 200, JSON_UNESCAPED_SLASHES);
-
+        } else {
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
+        }
+    } catch (\GuzzleHttp\Exception\BadResponseException $exception) {
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+        $result['contextWrites']['to']['status_msg'] = json_decode($exception->getResponse()->getBody());
+    }
+    return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
 });
